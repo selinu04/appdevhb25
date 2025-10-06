3cbfa3dcae5b5b611be105e1a521bbca698da1be
selinu04 <selin.ulutuerk@blum.com>
2025-10-06 10:41:08 +0200

    Aufgaben Woche 1

--- src/SelinUlutuerk/Program.cs
+++ src/SelinUlutuerk/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace Appdevhb25.SelinUlutuerk
+{
+    public class Programm
+    {
+        public static void Main(string[] args)
+        {
+            //Aufgabe01.PrintA.Start();
+            //Aufgabe02.Mahlreihen.Start();
+            //Aufgabe03.SummeUeberBereich.Start();
+            //Aufgabe04.Teilbarkeit.Start();
+            //Aufgabe05.PrintLine.Start();
+            //Aufgabe06.PrintRectangle.Start();
+            //Aufgabe07.PrintEmptySquare.Start();
+            //Aufgabe08.PrintTriangle.Start();
+            //Aufgabe09.PrintTriangle2.Start();
+            //Aufgabe10.PrintTriangle.StartPrintTriangle(8);
+            //Console.Write(Aufgabe11.CheckPrim.StartCheckPrim(7));
+            //Aufgabe12.Fibonacci.StartFibonacci(10);
+
+
+            //Bonusaufgabe01.PrintDeltoid.Deltoid(7);
+            //Bonusaufgabe02.PrintCircle.Start();
+            //Bonusaufgabe03.PrintChristmasTree.StartPrintChristmasTree(10);
+            //Bonusaufgabe04.HigherOrLower.RandomNumber();
+            Bonusaufgabe05.PrimitiveDatentypen.Start();
+        }
+    }
+}
